b8e64a380018ae80dc3ff31b9a57b6caf7aa43f9
dogukan <dogukan.matul@gmail.com>
2022-02-08 16:21:40 +0300

    Entityler OLuşturuldu

---
+++ StudentLibary.Core/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary.Core.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int IsbnNo { get; set; }
+        public string Name { get; set; }
+        public int Page { get; set; }
+       
+        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
+
+        public Author Author { get; set; }
+        public ICollection<Operation> Operations { get; set; }
+
+    }
+}
---
+++ StudentLibary.Core/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary.Core.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Surname { get; set; }
+        public string Class { get; set; }
+        public int StudentNo { get; set; }
+
+        public ICollection<Operation> Operations { get; set; }
+    }
+}
