88d1c3961dc0528d75d57def9c0d5d1195340578
dogukan <dogukan.matul@gmail.com>
2022-02-11 14:05:05 +0300

    Unit of Work sınıfı oluşturuldu

---
+++ StudentLibary.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using StudentLibrary.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IAuthorRepository Authors { get; }
+        IBookRepository Books { get;}
+        IStudentRepository Students { get; }
+        IOperationRepository Operations { get; }
+        Task<int> SaveAsync();
+    }
+}
---
+++ StudentLibary.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using StudentLibrary.Core.Repositories;
+using StudentLibrary.Core.UnitOfWork;
+using StudentLibrary.Data.Contexts;
+using StudentLibrary.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StudentLibraryContext _context;
+        private AuthorRepository _authorRepository;
+        private BookRepository _bookRepository;
+        private StudentRepository _studentRepository;
+        private OperationRepository _operationRepository;
+
+        public UnitOfWork(StudentLibraryContext context)
+        {
+            _context = context;
+        }
+
+        public IAuthorRepository Authors => _authorRepository ?? new AuthorRepository(_context);
+
+        public IBookRepository Books => _bookRepository ?? new BookRepository(_context);
+
+        public IStudentRepository Students => _studentRepository ?? new StudentRepository(_context);
+
+        public IOperationRepository Operations => _operationRepository ?? new OperationRepository(_context);
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
