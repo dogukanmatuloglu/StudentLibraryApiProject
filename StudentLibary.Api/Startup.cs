34baaa95e615210975319c132baa24f7456abe10
dogukan <dogukan.matul@gmail.com>
2022-02-26 15:01:07 +0300

    Stratup sınıfında hizmetler dahil edildi

--- StudentLibary.Api/Startup.cs
+++ StudentLibary.Api/Startup.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using StudentLibrary.Core.Repositories;
-
+using StudentLibrary.Core.Services;
 using StudentLibrary.Core.UnitOfWork;
 using StudentLibrary.Data.Contexts;
 using StudentLibrary.Data.Repositories;
 using StudentLibrary.Data.UnitOfWork;
-
+using StudentLibrary.Service.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +22,13 @@ namespace StudentLibary.Api
         
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<StudentLibraryContext>();
+            services.AddScoped<IAuthorService, AuthorService>();
+            services.AddScoped<IBookService, BookService>();
+            services.AddScoped<IStudentService, StudentService>();
+            services.AddScoped<IOperationService, OperationService>();
            
         }
 
