a5df80fead9c21d1aa5629d9b4b1dba50219d5e9
dogukan <dogukan.matul@gmail.com>
2022-03-01 08:19:19 +0300

    OperationController GetAll ve GetByID endopointleri tamamlandı

--- StudentLibary.Api/Controllers/OperationController.cs
+++ StudentLibary.Api/Controllers/OperationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StudentLibrary.Core.Dtos;
 using StudentLibrary.Core.Services;
 using System;
 using System.Collections.Generic;
@@ -18,5 +19,19 @@ namespace StudentLibrary.Api.Controllers
         {
             _operationService = operationService;
         }
+        public async Task<IActionResult> GetAll()
+        {
+            var operations = await _operationService.GetAllAsync();
+            return Ok(operations);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var operation = await _operationService.GetByIdAsync(id);
+            return Ok(operation);
+        }
+
+        
+
     }
 }
