e9b664f76beb8b4025aef070c6c7c885ae6dd2ba
dogukan <dogukan.matul@gmail.com>
2022-03-01 08:21:06 +0300

    OperationController Tammalandı

--- StudentLibary.Api/Controllers/OperationController.cs
+++ StudentLibary.Api/Controllers/OperationController.cs
@@ -30,8 +30,35 @@ namespace StudentLibrary.Api.Controllers
             var operation = await _operationService.GetByIdAsync(id);
             return Ok(operation);
         }
+        public async Task<IActionResult> Count()
+        {
+            int count = await _operationService.CountAsync();
+            return Ok(count);
 
-        
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(OperationAddDto operationAddDto)
+        {
+            await _operationService.AddAsync(operationAddDto);
+            return Created(string.Empty, operationAddDto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(OperationUpdateDto operationUpdateDto)
+        {
+            await _operationService.UpdateAsync(operationUpdateDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+
+            await _operationService.DeleteAsync(id);
+            return NoContent();
+        }
+
+
 
     }
 }
